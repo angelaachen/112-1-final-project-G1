ff539952f54df022b80a4ae84062ed8c94a5486f
angelaachen <144882700+angelaachen@users.noreply.github.com>
2023-12-13 14:37:01 +0800

    Add files via upload

--- project-1.R
+++ project-1.R
@@ -1,9 +1,31 @@
+#前置作業----
 library(readr)
-data <- jsonlite::fromJSON("data.json")
-class(data)
-on_stop_id <- data$on_stop_id
-class(on_stop_id)
-table(on_stop_id)
-data$on_stop_id[[527]]
 library(dplyr)
-test <- arrange(data,on_stop_id)
+data <- jsonlite::fromJSON("data.json")
+
+#命名Values----
+on_stop <- data$on_stop
+off_stop <- data$off_stop
+sum_of_txn_times <- data$sum_of_txn_times
+data_arranged<- dplyr::arrange(data,"on_stop")
+
+#改變data$sum_of_txn_times屬性----
+data$sum_of_txn_times <- as.numeric(data$sum_of_txn_times)
+
+#sum up交易次數by on/off stop----
+sum_of_txn_times_numbered_on_stop <- 
+  data |> dplyr::group_by(on_stop) |>
+  dplyr::summarise(
+sum_of_txn_times = sum(sum_of_txn_times)
+  ) 
+
+sum_of_txn_times_numbered_off_stop <- 
+  data |> dplyr::group_by(off_stop) |>
+  dplyr::summarise(
+    sum_of_txn_times = sum(sum_of_txn_times)
+  ) 
+
+#gg----
+
+
+
